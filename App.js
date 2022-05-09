8b051511b7e12f20cebf2437888d6acafbc499b4
Krishna <nayak100krishna@gmail.com>
2022-05-09 20:59:16 +0530

    Bottom Navigation Added

--- App.js
+++ App.js
@@ -8,18 +8,65 @@ import {
   Text,
   View,
 } from 'react-native';
+
 import Icon from 'react-native-vector-icons/Ionicons';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/screen/Home';
+import Search from './src/screen/Search';
+import Activity from './src/screen/Activity';
+import Profile from './src/screen/Profile';
+import Reels from './src/screen/Reels';
+
 const App = () => {
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
+  const BottomTabScreen = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarStyle: {
+            height: 50,
+          },
+
+          tabBarIcon: ({focused, size, color}) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused ? 'home-sharp' : 'home-outline';
+              size = focused ? size + 8 : size + 2;
+            } else if (route.name === 'Search') {
+              iconName = focused ? 'search' : 'ios-search-outline';
+            } else if (route.name === 'Reels') {
+              iconName = focused
+                ? 'caret-forward-circle'
+                : 'caret-forward-circle-outline';
+            } else if (route.name === 'Activity') {
+              iconName = focused ? 'ios-heart' : 'ios-heart-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'ios-person-circle' : 'ios-person-outline';
+            }
+            return <Icon name={iconName} size={size} color={'#000'} />;
+          },
+        })}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="Activity" component={Activity} />
+        <Tab.Screen name="Reels" component={Reels} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    );
+  };
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={'light-content'} />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-          <Text style={{fontFamily: 'Lobster-Regular'}}>Hello World</Text>
-          <Icon name="play" size={24} />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Bottom" component={BottomTabScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
