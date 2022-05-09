ca32f704a40fc4f7fa6f9f7534f9bc4dd5a3d92e
Krishna <nayak100krishna@gmail.com>
2022-05-09 20:21:35 +0530

    Components Added

---
+++ src/screen/Activity.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const Activity = () => {
+  return (
+    <View>
+      <Text>Activity Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Activity;
---
+++ src/screen/Reels.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Reels = () => {
+  return (
+    <View>
+      <Text>Reels Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Reels;
